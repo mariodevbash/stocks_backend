090455a01576b390137936d079ada7b7370d1674
Mario Ochoa <mario120gdl@gmail.com>
2022-02-28 17:33:32 -0600

    Models

--- src/test/unit/stocks.test.js
+++ src/test/unit/stocks.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const { app } = require('../../index')
+const { app } = require('../../../index')
 
 describe('POST /stocks', () => {
   test('Practice Test', () => {
